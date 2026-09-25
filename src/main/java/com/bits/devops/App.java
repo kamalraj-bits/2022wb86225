9183310ec6bd2d4964b0e456949d9c046f401f55
Kamalraj Thangaraj <2022wb86225@wilp.bits-pilani.ac.in>
2026-09-25 11:33:33 +0530

    Modify source code to test Jenkins Poll SCM

--- src/main/java/com/bits/devops/App.java
+++ src/main/java/com/bits/devops/App.java
@@ -4,6 +4,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("DevOps Continuous Integration Lab");
         System.out.println("BITS ID: 2022WB86225");
-        System.out.println("Initial Maven build completed successfully.");
+        System.out.println("Source code modified for Poll SCM testing.");
+        System.out.println("Jenkins continuous integration build triggered successfully.");
     }
 }
